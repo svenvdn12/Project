088ec8d7dc10700fc76fe14d86dc03a50b750244
spppie <s.visser@rcnheliomare.nl>
2025-09-15 13:40:30 +0200

    footer / header components

---
+++ views/header.php
@@ -0,0 +1,10 @@
+<header>
+    <ul id="header-list">
+        <li><a href="">Ontmoetingsgroep</a></li>
+        <li><a href="">Evenementen</a></li>
+        <li><a href="">Over Ons</a></li>
+        <li><a href="">Contact</a></li>
+        <li><a href="">FAQ</a></li>
+        <li><a href="">Nieuws</a></li>
+    </ul>
+</header>
