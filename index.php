a7c1dc5caf2bfba1ca398ea4cc67daed89c90816
spppie <s.visser@rcnheliomare.nl>
2025-09-15 13:40:19 +0200

    index met includes naar components

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+    declare(strict_types=1);
+?>
+<!DOCTYPE html>
+<html lang="">
+    <head>
+        <meta charset="UTF-8">
+        <title>php base</title>
+        <meta name="description" content="php base">
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <link rel="stylesheet" href="css/stylesheet.css">
+        <script src="js/script.js" defer></script>
+    </head>
+    <body>
+        <div id="container">
+            <?php include_once('views/header.php'); ?>
+            <main>
+            </main>
+            <?php include_once('views/footer.php'); ?>
+        </div>
+    </body>
+</html>
