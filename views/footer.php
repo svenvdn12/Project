920d99a360bde234a0bd1d8f1bfaa7cc8c8e8ec3
AdmiralGamerCat <hidde.j.p.aalders@gmail.com>
2025-09-16 14:03:29 +0200

    working on modules styling

--- views/footer.php
+++ views/footer.php
@@ -1,29 +1,36 @@
 <footer>
-    <section class="footer-section">
-        <h4>Pagina's</h4>
-        <ul id="footer-list">
-            <li>Ontmoetingsgroep</li>
-            <li>Evenementen</li>
-            <li>Over ons</li>
-            <li>FAQ</li>
-            <li>Nieuws</li>
-        </ul>
-    </section>
-    <section id="footer-contant" class="footer-section">
-        <h4>Contact</h4>
-        <p><a href="mailto:hetchaosatelier@gmail.com">hetchaosatelier@gmail.com</a></p>
-        <p><a href="tel:+31617177640">+31 6 17177640</a></p>
-        <section id="footer-contact-icon-link">
-            <a class="footer-link-svg" href="https://www.instagram.com/hetchaosatelier/"><img src="./assets/icons/brands/social/instagram.svg" alt="" width="30" height="30"></a>
-            <a class="footer-link-svg" href="https://www.linkedin.com/in/sofia-van-der-knaap-4848062a5/"><img src="./assets/icons/brands/social/linked-in.svg" alt=""></a>
+    <section class="flex col">
+        <section class="footer-section flex col">
+            <h2>Pagina's</h2>
+            <ul id="footer-list">
+                <li>Ontmoetingsgroep</li>
+                <li>Evenementen</li>
+                <li>Over ons</li>
+                <li>FAQ</li>
+                <li>Nieuws</li>
+            </ul>
         </section>
-    </section>
-    <section class="footer-section">
-        <p>Wil je op de hoogte blijven dan de activiteiten en evenementen van het Chaos Atelier?</p>
-        <p>Schrijf je in voor onze maandelijkse nieuwsbrief!</p>
-        <form action="#">
-            <input type="email" name="email" id="footer-form-email" placeholder="E-mailadres...">
-            <input type="submit" value="JOIN" id="footer-form-join">
-        </form>
+        <section id="footer-content" class="footer-section flex col">
+            <h2>Contact</h4>
+            <p><a href="mailto:hetchaosatelier@gmail.com">hetchaosatelier@gmail.com</a></p>
+            <p><a href="tel:+31617177640">+31 6 17177640</a></p>
+            <section id="footer-contact-icon-link">
+                <a class="footer-link-svg" href="https://www.instagram.com/hetchaosatelier/"><img src="./assets/icons/brands/social/instagram.svg" alt="" width="30" height="30"></a>
+                <a class="footer-link-svg" href="https://www.linkedin.com/in/sofia-van-der-knaap-4848062a5/"><img src="./assets/icons/brands/social/linked-in.svg" alt=""></a>
+            </section>
+        </section>
+        <section class="footer-section text-center flex col">
+            <p>Wil je op de hoogte blijven dan de activiteiten en evenementen van het Chaos Atelier?</p>
+            <p>Schrijf je in voor onze maandelijkse nieuwsbrief!</p>
+            <form action="#">
+                <input
+                    type="email"
+                    name="newsletter-input"
+                    id="newsletter-input"
+                    placeholder="E-mailadres..."
+                />
+                <input type="submit" value="JOIN" class="btn">
+            </form>
+        </section>
     </section>
 </footer>
