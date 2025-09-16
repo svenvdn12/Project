7d56c94232bef082cee450648316577f5345c442
AdmiralGamerCat <hidde.j.p.aalders@gmail.com>
2025-09-16 21:35:13 +0200

    completed footer module css for mobile and desktop. Changed some styles in 'main.css' and 'header.css'

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="stylesheet" href="css/main.css">
         <link rel="stylesheet" href="css/modules/header.css">
+        <link rel="stylesheet" href="css/modules/footer.css">
         <script src="js/header-dropdown-menu-toggle.js" defer></script>
     </head>
     <body>
@@ -17,6 +18,21 @@
             <?php include_once('views/header.php'); ?>
             <div id="page-content-container">
                 <main>
+                    <section>
+                        <header>
+                            <h1>TEST HEADING</h1>
+                            <img src="assets/icons/megaphone.svg" alt="megaphone icon">
+                        </header>
+                    </section>
+                    <div class="test-block">
+                        <h1>TEST BLOCK PLEASE REMOVE</h1>
+                    </div>
+                    <div class="test-block">
+                        <h1>TEST BLOCK PLEASE REMOVE</h1>
+                    </div>
+                    <div class="test-block">
+                        <h1>TEST BLOCK PLEASE REMOVE</h1>
+                    </div>
                 </main>
                 <?php include_once('views/footer.php'); ?>
             </div>
--- views/footer.php
+++ views/footer.php
@@ -1,34 +1,31 @@
-<footer>
-    <section id="footer-wrapper">
-        <section class="footer-section flex col">
+<footer id="page-footer">
+    <section class="footer-content">
+        <section class="footer-col">
             <h2>Pagina's</h2>
-            <ul id="footer-list">
-                <li>Ontmoetingsgroep</li>
-                <li>Evenementen</li>
-                <li>Over ons</li>
-                <li>FAQ</li>
-                <li>Nieuws</li>
+            <ul>
+                <li><a href="#">Ontmoetingsgroep</a></li>
+                <li><a href="#">Evenementen</a></li>
+                <li><a href="#">Over ons</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Nieuws</a></li>
             </ul>
         </section>
-        <section id="footer-content" class="footer-section flex col">
-            <h2>Contact</h4>
-            <p><a href="mailto:hetchaosatelier@gmail.com">hetchaosatelier@gmail.com</a></p>
-            <p><a href="tel:+31617177640">+31 6 17177640</a></p>
-            <section id="footer-contact-icon-link">
-                <a class="footer-link-svg" href="https://www.instagram.com/hetchaosatelier/"><img src="./assets/icons/brands/social/instagram.svg" alt="" width="30" height="30"></a>
-                <a class="footer-link-svg" href="https://www.linkedin.com/in/sofia-van-der-knaap-4848062a5/"><img src="./assets/icons/brands/social/linked-in.svg" alt=""></a>
-            </section>
+        <section class="footer-col">
+            <h2>Contact</h2>
+            <ul>
+                <li><a href="mailto:hetchaosatelier@gmail.com">hetchaosatelier@gmail.com</a></li>
+                <li><a href="tel:+31 6 17177640">+31 6 17177640</a></li>
+            </ul>
+            <div class="socials-wrapper">
+                <a href="#"><img src="assets/icons/brands/social/linked-in.svg" alt="Linked In logo"></a>
+                <a href="#"><img src="assets/icons/brands/social/instagram.svg" alt="Instagram logo"></a>
+            </div>
         </section>
-        <section class="footer-section text-center flex col">
-            <p>Wil je op de hoogte blijven dan de activiteiten en evenementen van het Chaos Atelier?</p>
+        <section class="footer-col">
+            <p>Wil je op de hoogte blijven van de activiteiten en evenementen van het Chaos Atelier?</p>
             <p>Schrijf je in voor onze maandelijkse nieuwsbrief!</p>
             <form action="#">
-                <input
-                    type="email"
-                    name="newsletter-input"
-                    id="newsletter-input"
-                    placeholder="E-mailadres..."
-                />
+                <input type="email" name="newsletter-email-input" id="newsletter-email-input" placeholder="E-mailadres..." required>
                 <input type="submit" value="JOIN" class="btn">
             </form>
         </section>
