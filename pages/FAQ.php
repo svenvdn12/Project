8d594c796b46f0a24d7aa5a88b657b84acf5207c
Aefrir <daanphin@gmail.com>
2025-09-18 13:48:07 +0200

    bug fixes on faq
    details closed by default, min width set on main

--- pages/FAQ.php
+++ pages/FAQ.php
@@ -27,7 +27,7 @@
                     </section>
                     <section class="faq-content">
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Waar is het Chaos Atelier?</h2></summary>
                           <p>Het Chaos Atelier heeft geen vaste bezoeklocatie, we werken voornamelijk vanuit het kantoor van STAD Leiden, op de Morsweg 55E-1.</p>
                           <p>Voor de activiteiten werken we zo veel mogelijk samen met andere partijen en zullen dus op veel verschillende locaties - door Leiden heen - plaatsvinden.</p>
@@ -35,44 +35,43 @@
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Voor wie?</h2></summary>
                           <p>Het Chaos Atelier is een programma voor jongvolwassenen die zich herkennen in neurodivergentie. Daaronder verstaan wij jongeren van ongeveer 18 - 30.</p>
                           <p>Mocht je twijfelen of één van de activiteiten iets voor jou is, kun je altijd een kennismakingsgesprek inplannen. Dan kijken we samen of jouw verwachtingen en wensen een match zijn met wat wij aanbieden.</p>
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Hoe meld ik me aan?</h2></summary>
                           <p>Als je interesse hebt in een activiteit kan je via de activiteitenpagina het aanmeldformulier invullen.</p>
                           <p>Weet je nog niet waar je aan wilt deelnemen? We denken graag met je mee! Stuur ons een berichtje zodat we een kennismakingsgesprek kunnen inplannen.</p>
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Ik heb geen bevestiging gekregen?</h2></summary>
                           <p>Heb je geen bevestigingsbericht gekregen van jouw deelname? Check dan even je spambox, of stuur ons een berichtje.</p>
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Hoe meld ik me af?</h2></summary>
                           <p>Afmelden kan via het e-mailadres of telefoonnummer waar je ook het bevestigingsbericht van hebt ontvangen.</p>
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Wie organiseert het?</h2></summary>
                           <p>Team Chaos werkt samen met verschillende partijen voor het organiseren van alle workshops, activiteiten en evenementen. Op de activiteitenpagina zie je per activiteiten de betrokkenen.</p>
                         </details>
                       </article>
                       <article>
-                        <details open>
+                        <details>
                           <summary><h2>Wat zijn de kosten?</h2></summary>
                           <p>De activiteiten die het Chaos Atelier organiseert zijn zo veel mogelijk gratis toegankelijk. Mochten er wel kosten verbonden zitten aan deelname, dan wordt dat vermeld op de activiteitenpagina.</p>
                         </details>
                       </article>
-                      
                     </section>
                 </main>
                 <?php include_once('../views/footer.php'); ?>
