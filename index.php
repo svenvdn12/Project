3e7e7226c990710da249f299916490eee24d3b74
spppie <s.visser@rcnheliomare.nl>
2025-09-24 14:52:23 +0200

    einde dag, basis/concept staat

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
 <?php
     declare(strict_types=1);
+
+    if (!isset($_GET['page'])) {   
+        $_GET['page'] = 'home';
+    }
+    switch($_GET['page']) {
+        case 'home':
+            $include = 'home';
+            break;
+        case 'FAQ':
+            $include = 'FAQ';
+            break;
+        default:
+            $include = 'home';
+    }
+
 	$allowedUrls = 
 	[
 		'contact',
@@ -21,20 +36,24 @@
         <?php include_once('views/styles.php');?>
 		<script src="js/spa-menu-function.js" defer></script>
 	</head>
-	<body id="spa">
+	<body>
         <?php include_once 'views/header.php';?>
         <main id="main" class="scroll-anchor"> <!-- Het overkoepelende element dat alle pagina's bevat MOET een scroll-anchor zijn met de id 'main' -->
             <?php include_once 'views/menu.php';?>
             <aside id="start-point"></aside>
             <div class="scroll-anchor plx plx-component"> <!-- De content container van elke pagina moet deze classes hebben -->
-                <?php include_once('pages/home.php'); ?>
+                <?php include('pages/home.php'); ?>
             </div>
             <aside class="scroll-anchor stop"><h2>FAQ</h2></aside> <!-- Vul de H2 met de naam van de volgende pagina-->
             <aside class="divider"></aside> <!-- Deze twee elementen moeten tussen elke content container, behalve tussen het hamburger menu en de welkomstpagina -->
             <div id="over-ons" class="scroll-anchor plx plx-component">
-                <?php include_once('pages/FAQ.php') ?>
+                <?php include('pages/FAQ.php') ?>
             </div>
-            <?php include_once 'views/footer.php';?>
+            <?php include('views/footer.php');?>
+        </main>
+        <main id="main-2">
+            <?php include 'pages/'.$include.'.php'?>
+            <?php include('views/footer.php');?>
         </main>
 	</body>
 </html>
--- views/header.php
+++ views/header.php
@@ -1,11 +1,6 @@
 <header id="page-header">
     <section class="header-top">
         <a href="./pages/home.php"><img src="./assets/icons/brands/site/logo.svg" alt="Chaos Atelier logo"></a>
-         <!-- <a href="home.php">
-            <object data="./assets/icons/brands/site/logo.svg" type="image/jpg">
-                <img src="../assets/icons/brands/site/logo.svg"/>
-            </object>
-         </a> -->
         <label for="hamburger-toggle" class="hamburger-toggle-wrapper">
             <input type="checkbox" name="hamburger-toggle" id="hamburger-toggle">
             <span class="hamburger-icon">
@@ -14,5 +9,17 @@
                 <span class="line bottom"></span>
             </span>
         </label>
+    </section>
+    <section id="header-dropdown-menu">
+        <ul>
+            <li><a href="index.php?page=home">home</a></li>
+            <li><a href="../pages/info.php">Ontmoetingsgroep</a></li>
+            <li><a href="../pages/evenementen.php">Evenementen</a></li>
+            <li><a href="../pages/over-ons.php">Over ons</a></li>
+            <li><a href="../pages/contact.php">Contact</a></li>
+            <li><a href="index.php?page=FAQ">FAQ</a></li>
+            <li><a href="../pages/nieuws.php">Nieuws</a></li>
+            <!-- might be wrong ^ -->
+        </ul>
     </section>
 </header>
