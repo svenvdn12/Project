573dfa4ee0945dfdb24d9bb428c4911f1a88f3ab
Aefrir <daanphin@gmail.com>
2025-09-25 13:22:20 +0200

    bug fix on info page

--- pages/info.php
+++ pages/info.php
@@ -1,14 +1,14 @@
 <section>
     <header id="content-header">
         <h1>ONTMOETINGS-GROEP</h1>
-        <img src="../assets/icons/megaphone.svg" alt="megaphone icon">
+        <img src="./assets/icons/megaphone.svg" alt="megaphone icon">
     </header>
     <p>placeholder datum en locatie</p>
     <p>De ontmoetingsgroep is een serie van 10 bijeenkomsten, waar we steeds over een ander thema in gesprek gaan. Denk aan emoties, keuzes maken en identiteit. We zorgen samen voor een veilige omgeving, waaring het uitwisselen van ervaringen centraal staat.</p>
     <p>De ontmoetingsgroep is voor jongvolwassenen (plusminus 18 - 30 jaar) die zich herkenne in neurodivergentie en graag in gesprek gaan met gelijkgestmden</p>
     <p>Of je nou kort geleden gediagnosticeerd bent en opzoek bent naar wat dat voor jou betekent, je niks met labels hebt en gewoon iets wil doen met die chaos in je hoofd, of een doorgewinterde neurodivergent bent die graag diens wijsheid deelt.</p>
     <p>Ik vraag NIET om een officiële diagnose.</p>
-    <img src="../assets/images/placeholder.jpg" alt="placeholder" width="300" id="info-image">
+    <img src="./assets/images/placeholder.jpg" alt="placeholder" width="300" id="info-image">
     <p>Onderdeel van de aanmelding is een kennismakingsgesprek met de host. Zo weten jullie beiden met wie je te maken hebt en bespreken jullie al jouw verwachtingen en wensen rondom de ontmoetingsgroep.</p>
     <p>Schrijf je hieronder in! Er wordt contact met je opgenomen voor het maken van een afspraak.</p>
     <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" id="meeting-form" method="post">
