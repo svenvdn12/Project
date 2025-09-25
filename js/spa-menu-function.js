d3d872d301854fa4c3617d895713c00841e49c82
Raiyen "JM" Janszen <janszen@galactis-group.com>
2025-09-25 14:39:32 +0200

    laatste sass compilatie + menu fixes

--- js/spa-menu-function.js
+++ js/spa-menu-function.js
@@ -1,12 +1,15 @@
 'use strict';
 document.querySelector('#start-point').scrollIntoView();
 
+document.querySelector('#main').style.scrollBehavior = 'smooth';
+
 const button = document.querySelector('#hamburger-toggle');
 let isOpen = false;
 
 button.addEventListener('click', function () {
 	if (isOpen) {
 		document.querySelector('#start-point').scrollIntoView({ behavior: 'smooth' });
+		document.querySelector('#hamburger-toggle').removeAttribute('checked');
 		isOpen = false;
 	} else {
 		document.querySelector('.menu').scrollIntoView({ behavior: 'smooth' });
--- views/menu.php
+++ views/menu.php
@@ -3,7 +3,6 @@
         <li><a href="#info">Ontmoetingsgroep</a></li>
         <li><a href="#evenementen">Evenementen</a></li>
         <li><a href="#over-ons">Over ons</a></li>
-        <li><a href="#contact">Contact</a></li>
         <li><a href="#nieuws">Nieuws</a></li>
         <li><a href="#FAQ">FAQ</a></li>
         <!-- might be wrong ^ -->
